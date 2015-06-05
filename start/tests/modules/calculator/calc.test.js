cb49349db2cc2853beb8daa95a5edc058c073ab7
Oscar Lodriguez <o.lodriguez@gmail.com>
2015-06-05 07:44:08 +0200

    testing

---
+++ start/tests/modules/calculator/calc.test.js
@@ -0,0 +1,24 @@
+/**
+ * @list of usable assertions
+ *
+ * ok(state, message); Truthful assertion
+ * equal(actual, expected, message); Compares two values and check if they are the same
+ * notEqual(actual, expected, message); Compare two values for inequality
+ * deepEqual(actual, expected, message); Compare complex objects for equality (arrays, objects)
+ * notDeepEqual(actual, expected, message); Compare complex objects for inequality (arrays, objects)
+ * strictEqual(actua, expected, message); Compare objects as the with strict type '===' default
+ * notStrictEqual(actual, expected, message); Compare objects as the with strict type '===' inequality
+ *
+ *
+ *
+ * @list of events on Qunit
+ * Log
+ * testStart
+ * testDone
+ * test
+ * module
+ * moduleStart
+ * moduleDone
+ * Begin
+ * done
+ */
